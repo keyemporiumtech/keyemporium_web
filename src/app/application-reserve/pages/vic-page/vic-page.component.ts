37180dce36f5549c583845c7455d8b38cdf65b34
giusasso <keyemporium.tech@gmail.com>
2023-01-19 14:58:42 +0100

    Estensione base page per gestione permissions

--- src/app/application-reserve/pages/vic-page/vic-page.component.ts
+++ src/app/application-reserve/pages/vic-page/vic-page.component.ts
@@ -1,25 +1,17 @@
 import { Component } from '@angular/core';
-import {
-	ApplicationLoggerService,
-	ApplicationStorageService,
-	BasePageComponent,
-	PageUtility,
-} from '@ddc/kit';
-import { Router, ActivatedRoute, Data, ParamMap } from '@angular/router';
+import { ActivatedRoute, Data, ParamMap, Router } from '@angular/router';
+import { ApplicationLoggerService, ApplicationStorageService, PageUtility } from '@ddc/kit';
+import { KeBasePageComponent } from '../../../application-shared/components/ke-base-page.component';
 import { AuthenticationService } from '../../../modules/authentication/base/authentication.service';
-import { EnumPermissions } from '../../../application-shared/constants/permissions.enum';
 
 @Component({
 	selector: 'reserve-vic-page',
 	templateUrl: './vic-page.component.html',
 	styleUrls: ['./vic-page.component.scss'],
 })
-export class VicPageComponent extends BasePageComponent {
+export class VicPageComponent extends KeBasePageComponent {
 	// var
-	queryParams: ParamMap;
 	blocked: boolean;
-	id_user: string;
-	idUserLogged: string;
 	// qr-code
 	textQr: string;
 	// sizes
@@ -32,14 +24,10 @@ export class VicPageComponent extends BasePageComponent {
 		applicationLogger: ApplicationLoggerService,
 		router: Router,
 		activatedRoute: ActivatedRoute,
-		private applicationStorage: ApplicationStorageService,
-		private authenticationService: AuthenticationService,
+		applicationStorage: ApplicationStorageService,
+		authenticationService: AuthenticationService,
 	) {
-		super(applicationLogger, router, activatedRoute);
-		this.idUserLogged = this.applicationStorage.userId.get();
-		if (this.queryParams) {
-			this.setParams(this.queryParams);
-		}
+		super(applicationLogger, router, activatedRoute, applicationStorage, authenticationService);
 	}
 
 	ngOnInitForChildren() {
@@ -57,29 +45,17 @@ export class VicPageComponent extends BasePageComponent {
 	manageDataParams(data: Data) {}
 	manageRouteParams(data: ParamMap) {}
 	manageQueryParams(data: ParamMap) {
-		this.queryParams = data;
-		if (this.idUserLogged) {
-			this.setParams(data);
-		}
+		super.manageQueryParams(data);
 	}
 
-	private setParams(data: ParamMap) {
-		if (data.has('ID_USER')) {
-			this.id_user = PageUtility.decodeParam(data.get('ID_USER'));
-			if (
-				this.id_user !== this.idUserLogged &&
-				!this.authenticationService.checkPermissions([
-					EnumPermissions.SUPERVISOR,
-					EnumPermissions.CHANGE_PROFILES,
-				])
-			) {
-				this.blocked = true;
-			}
-		} else {
-			this.id_user = this.idUserLogged;
-		}
+	// ----- inherit
+	workParams(data: ParamMap) {
 		// qr-code
 		this.textQr = window.location.host + '/#/reserve/vic/' + PageUtility.encodeParam(this.id_user);
+	}
+	declarePermissions(): Map<string, string[]> {
+		const mapPermissions: Map<string, string[]> = new Map<string, string[]>();
+		return mapPermissions;
 	}
 
 	// manager size
